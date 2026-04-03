ee6681e9ae7f46a8998ac89c960cd66e7b00c5e7
ranabrid <aleksandra_khlebnikova@bk.ru>
2026-04-04 01:56:50 +0300

    Update config.py

--- FlowerRemBot/config.py
+++ FlowerRemBot/config.py
@@ -12,10 +12,6 @@ DATABASE_NAME = os.getenv(
 )  # Можно задать значение по умолчанию
 MOSCOW_TIMEZONE = os.getenv("MOSCOW_TIMEZONE", "Europe/Moscow")
 
-# Настройки Webhook
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-SECRET_TOKEN = os.getenv("SECRET_TOKEN")  # Если используете
-
 # Проверка наличия обязательных переменных
 if not TELEGRAM_BOT_TOKEN:
     raise ValueError("TELEGRAM_BOT_TOKEN must be set")
